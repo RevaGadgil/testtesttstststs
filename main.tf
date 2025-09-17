d1aa4d83002c49d281a4364cebce7cac2d023bdd
RevaGadgil <gadgilreva@gmail.com>
2025-09-17 10:30:52 +0200

    maintf changes

--- main.tf
+++ main.tf
@@ -1,6 +1,4 @@
 provider "aws" {
-  region  = "eu-west-3"
-<<<<<<< HEAD
   profile = "default"
   version = "~> 3.0"
 
@@ -8,25 +6,11 @@ provider "aws" {
 
 resource "aws_s3_bucket" "s3bucket" {
   bucket = "bigbucketgHH"
-  acl    = "private"
 
   tags = {
     Name        = "My bucket"
     Environment = "Dev"
   }
 
-  versioning {
-    enabled = true
-  }
-
-
-}
-=======
-  version = "~>6.0"
 }
 
-resource "aws_s3_bucket" "s3bucket" {
-  bucket = "mzbucket2025GZ"
-  acl    = "private"
-}
->>>>>>> 5de4cb889961a380bde4a1477e6eb660cbcb12f6
